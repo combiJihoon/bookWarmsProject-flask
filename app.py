05e4953768aba9eb9e4a64ac40ce71bbaac06711
combiJihoon <combi_jihoon@naver.com>
2021-08-11 15:07:06 +0900

    Add csrf_token

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect  # csrf
+
+from db_connect import db
 import config
 import secrets
-
-db = SQLAlchemy()
 
 
 def create_app():
@@ -12,12 +12,15 @@ def create_app():
 
     app.config.from_object(config)
 
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
     db.init_app(app)
-    Migrate().init_app(app, db)
+    Migrate().init_app(app, db, render_as_batch=True)
 
     from views import main_view
     import model
-    app.register_blueprint(main_view.board)
+    app.register_blueprint(main_view.bp)
 
     # 세션 암호화 => 서버가 정보를 암호화 해서 가지고 있겠다는 뜻
     secret_key = secrets.token_hex(16)
